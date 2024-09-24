3658590931cc6c00511c8c3bf28379a519cf009b
George <george.lipceanu@gmail.com>
2024-09-24 12:26:40 +0100

    added Tasky.js and maded necessary changes to App.js

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -1,13 +1,15 @@
-
 import './App.css';
+import Task from './components/Task';
 
 function App() {
   return (
     <div className="App">
       <h1>Tasky</h1>
+      <Task title="Dishes" deadline="Today" />
+      <Task title="Laundry" deadline="Tomorrow" />
+      <Task title="Tidy" deadline="Today" />
     </div>
   );
 }
 
 export default App;
-
