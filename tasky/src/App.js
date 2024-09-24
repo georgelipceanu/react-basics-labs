7090a915b0ea2a08a85a0482bfdac7f55e53f825
George <george.lipceanu@gmail.com>
2024-09-24 12:35:41 +0100

    Completed Tasky Lab 1

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -6,10 +6,10 @@ function App() {
     <div className="container">
       <h1>Tasky</h1>
       <Task title="Dishes" deadline="Today">
-        Empty Dishwasher
+        Empty Dishwasher.
       </Task>
       <Task title="Laundry" deadline="Tomorrow">
-        Fold laundry and put away
+        Fold laundry and put away.
       </Task>
       <Task title="Tidy" deadline="Today" />
     </div>
