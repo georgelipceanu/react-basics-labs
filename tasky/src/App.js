ef9d5acb07cdd36d9f47c70ba4925211dd734348
George <george.lipceanu@gmail.com>
2024-09-30 19:42:28 +0100

    Add task: form added

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Task from './components/Task';
 import React, { useState } from 'react';
+import AddTaskForm from './components/Form';
 
 function App() {
   const [ taskState, setTaskState ] = useState({
@@ -37,7 +38,8 @@ function App() {
           markDone={() => doneHandler(index)}
           deleteTask = {() => deleteHandler(index)}
         />
-      ))} 
+      ))}
+      <AddTaskForm /> 
     </div>
   );
 }
